5862cc9759a2fbfbdd3e6fe6a26171b7938dfdda
Hirofumi Wakasugi <baenej@gmail.com>
2015-11-21 16:04:16 +0900

    Change gulp-sequence to run-sequence

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,9 +1,9 @@
-var gulp         = require('gulp');
-var gulpSequence = require('gulp-sequence')
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
 gulp.task('build', function(cb) {
   var tasks = ['clean',['iconFont', 'images'], ['sass', 'browserify']];
   if(process.env.RAILS_ENV === 'production') tasks.push('rev');
   tasks.push(cb);
-  gulpSequence.apply(this, tasks);
+  return runSequence.apply(this, tasks);
 });
